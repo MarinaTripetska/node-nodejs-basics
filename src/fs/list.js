b5b7a9544d91d9bd0cc1dd444d52ea69cabe58df
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-06-19 22:23:50 +0200

    feat: resolve task fs/list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  try {
+    // Check if directory exists
+    await fs.access(join(__dirname, "files"));
+
+    const files = await fs.readdir(join(__dirname, "files"));
+    console.log(files);
+  } catch (error) {
+    // If directory does not exist, throw error
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
